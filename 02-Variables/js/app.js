b4a57ffad8a7e7fc3dd72d052575b4be78c0e702
unknown <albetho@gmail.com>
2023-01-03 00:33:02 -0500

    Variable ejemplo Lap

--- 02-Variables/js/app.js
+++ 02-Variables/js/app.js
@@ -1,3 +1,2 @@
-const nombre = "Roberto";
-
-alert(nombre);
+const nombre = prompt("¿Cual es tu nombre?");
+document.querySelector(".contenido").innerHTML = `${nombre} es ahora un trome en JavaScript`;
